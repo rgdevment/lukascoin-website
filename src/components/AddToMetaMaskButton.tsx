0d2d5dc43e2beae8ec013566d011e9316a2e60f3
rgdevment <mhidalgog@falabella.com>
2025-04-16 13:56:34 -0400

    styles: nomenclature on buttons

--- src/components/AddToMetaMaskButton.tsx
+++ src/components/AddToMetaMaskButton.tsx
@@ -42,7 +42,7 @@ const AddToMetaMaskButton: React.FC = () => {
 
   return (
     <button className="btn btn-metamask" onClick={addLKS}>
-      Agregar LKS a MetaMask
+      LKS en MetaMask
     </button>
   )
 }
